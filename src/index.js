4b86ad33152cb26b537d8cd72e7bc452fb0f6904
zido <wuhongxu1208@gmail.com>
2017-10-17 02:07:55 +0800

    some test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import './index.css'
-console.log('3456adw')
+import hljs from 'highlight.js'
+import 'highlight.js/styles/default.css'
+import marked from 'marked'
+hljs.initHighlightingOnLoad()
+console.log(marked('I am using __markdown__.'))
+
 if(module.hot){
   module.hot.accept()
 }
