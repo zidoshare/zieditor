8819a8df6432b9d0fc983dba461372c8c6caf92c
邬虹旭 <wuhongxu1208@gmail.com>
2018-01-30 16:59:21 +0800

    清除不必要依赖

--- config.js
+++
@@ -1,7 +0,0 @@
-const path = require('path')
-//一些配置项
-module.exports = {
-  srcPath: path.join(__dirname,'src'),
-  dist: path.join(__dirname,'dist'),
-  indexHtml: path.join(__dirname,'src/index.html'),
-}
