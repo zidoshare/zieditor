5f6a0cd9063408594222b5ae951295d4702a0374
zido <wuhongxu1208@gmail.com>
2018-07-26 18:30:29 +0800

    add some rules change

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -27,5 +27,6 @@ module.exports = {
   },
   resolve: {
     modules: ['node_modules', path.join(__dirname, 'node_modules')],
+    extensions: ['.js', '.ts', '.json', '.scss'],
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,7 @@
-import * as hljs from 'highlight.js'
-import { addClass, hasClass, removeClass } from './utils/className'
-let mdHtml, mdSrc, permallink, scrollMap
-export interface ZiMarkedOption {
-  html?: boolean,
-  xhtmlOut?: boolean,
-  breaks?: boolean,
-  langPrefix?: string,
-  linkify?: boolean,
-  linkTarget?: '',
-  typographer?: boolean,
-
-  _highlight?: boolean,
-  _strict?: boolean,
-  _view?: string,
-  hightlight?(str: string, lang: string): string
-}
+import * as hljs from 'highlight.js';
+import * as Remarkable from 'remarkable';
+import { addClass, hasClass, removeClass } from './utils/className';
+let mdHtml: Remarkable, mdSrc: Remarkable, permallink, scrollMap;
 let defaults = {
   html: false,
   xhtmlOut: false,
@@ -54,6 +41,41 @@ function setResultView(val: string) {
   removeClass(document.body, 'result-as-debug');
   addClass(document.body, 'result-as-' + val)
   defaults._view = val;
+}
+function mdInit() {
+  if (defaults._strict) {
+    mdHtml = new Remarkable('commonmark');
+    mdSrc = new Remarkable('commonmark');
+  } else {
+    mdHtml = new Remarkable('full', defaults);
+    mdSrc = new Remarkable('full', defaults);
+  }
+  mdHtml.renderer.rules.table_open = function () {
+    return '<table class="table table-striped">\n';
+  };
+  mdHtml.renderer.rules.paragraph_open = function (tokens: Remarkable.Token[], idx: number): string {
+    let line: number;
+    const lines = tokens[idx].lines;
+    if (lines && tokens[idx].level === 0) {
+      line = lines[0];
+      return `<p class="line" data-line="${line}">`;
+    }
+    return '<p>';
+  };
+
+  mdHtml.renderer.rules.heading_open=function(tokens:Remarkable.Token[],idx:number):string{
+    let line;
+    const lines = tokens[idx].lines;
+    if( lines && tokens[idx].level === 0){
+      line = lines[0]
+      return `<h${tokens[idx].hLevel} class="line" data-line="${line}"`;
+    }
+    return `<h${tokens[idx].hLevel}>`;
+  };
+}
+
+function setHighlightedContent(selector,content,lang){
+  
 }
 if (module.hot) {
   module.hot.accept()
